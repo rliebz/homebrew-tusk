498fea4a5c4bc9f604106336963e19b33d06cc43
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-16 14:54:22 -0400

    tusk version v0.1.1

--- tusk.rb
+++ tusk.rb
@@ -1,9 +1,9 @@
 class Tusk < Formula
   desc "A tusk runner built with simplicity in mind"
   homepage ""
-  url "https://github.com/rliebz/tusk/releases/download/v0.1.0/tusk_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "874fb28348a83ff3687bd246cc20a68eace69d78519d023b9ba30ce7feba07df"
+  url "https://github.com/rliebz/tusk/releases/download/v0.1.1/tusk_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "1b2f628303583aba0cf3bff3148bda65c8902b093436b44755b7c90bbc8516ef"
 
   def install
     bin.install "tusk"
