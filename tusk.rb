95625204b8925344a1b2bbd3ce0b35f8bacc1e62
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-31 10:22:34 -0400

    Brew formula update for tusk version v0.4.3

--- tusk.rb
+++ tusk.rb
@@ -1,9 +1,9 @@
 class Tusk < Formula
   desc "A tusk runner built with simplicity in mind"
   homepage "https://github.com/rliebz/tusk"
-  url "https://github.com/rliebz/tusk/releases/download/v0.4.2/tusk_0.4.2_darwin_amd64.tar.gz"
-  version "0.4.2"
-  sha256 "e6448a6413093cdf78cc61b2ed70c46811e53a8ef0f5fb630b3f1dc2277ae935"
+  url "https://github.com/rliebz/tusk/releases/download/v0.4.3/tusk_0.4.3_darwin_amd64.tar.gz"
+  version "0.4.3"
+  sha256 "bf5a8f31aa6005c24692ed2ed439f63da102365816e4582a91e7bc920dda6abe"
 
   def install
     bin.install "tusk"
